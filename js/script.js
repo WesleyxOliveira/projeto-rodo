a1f0e3b6d58f4725f4e8f44bc228c0189bd6de9f
Wesley De Souza Oliveira <Wesleyxoliveira@gmail.com>
2022-12-14 20:35:00 -0300

    finalizei media query

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+function mudouTamanho() {
+    if(window.innerWidth >= 768) {
+        itens.style.display = 'block';
+    } else {
+        itens.style.display = "none";
+    }
+}
 function showMenu() {
     if(itens.style.display == "none") {
         itens.style.display = "block";
