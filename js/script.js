edf671c62e9507343507ce39f71756aa20b4b800
Wesley De Souza Oliveira <Wesleyxoliveira@gmail.com>
2022-12-20 10:01:28 -0300

    atualizei a media query do projeto

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
-function mudouTamanho() {
-    if(window.innerWidth >= 768) {
-        itens.style.display = 'block';
-    } else {
-        itens.style.display = "none";
-    }
-}
 function showMenu() {
     if(itens.style.display == "none") {
         itens.style.display = "block";
     } else {
         itens.style.display = "none";
     }
+}
+
+function mudouTamanho() {
+    if(body.innerWidth ==)
 }
