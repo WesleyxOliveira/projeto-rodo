4bb2672fa4636522e650cf1bf5335d538acd8bf2
Wesley De Souza Oliveira <Wesleyxoliveira@gmail.com>
2022-12-20 20:04:42 -0300

    atualizei o projeto rodo

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function mudouTamanho() {
+    if(window.innerWidth >= 768) {
+        itens.style.display = "block";
+    } else {
+        itens.style.display = "none";
+    }
+}
+
+
+function clickMenu() {
+    if(itens.style.display == "none") {
+        itens.style.display = "block";
+    }else {
+        itens.style.display = "none";
+    }
+}
